2b98e4d682ed6e8e5f31cc3ffbf331fd46e10228
Anish <srtanish1992@gmail.com>
2019-09-29 00:48:04 -0700

    going

--- src/config/config.js
+++ src/config/config.js
@@ -2,14 +2,14 @@ let env = "local";
 
 let envs = {
   local: {
-    hostname: "157.245.209.85",
+    hostname: "localhost",
     port: 8080
   }
 };
 
 module.exports = {
   client: {
-    hostname: "157.245.209.85",
+    hostname: "localhost",
     port: "8080"
   },
   server: envs[env]
