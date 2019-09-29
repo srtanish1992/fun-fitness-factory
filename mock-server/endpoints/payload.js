c4f3ab05c29a8b4696a3f914ee9c526aa8f5b50f
Anish <srtanish1992@gmail.com>
2019-09-29 01:43:34 -0700

    port change

--- mock-server/endpoints/payload.js
+++ mock-server/endpoints/payload.js
@@ -54,7 +54,7 @@ module.exports = app => {
 
   app.get("/payload", (req, res) => {
     console.log("success");
-    axios.get("http://127.0.0.1:5984/payload/bd0fa39268f1f36899bbcbf81e000ba3").then(response => {
+    axios.get("http://157.245.209.85:5984/payload/bd0fa39268f1f36899bbcbf81e000ba3").then(response => {
       res.send({
         status: "Got payload",
         response: response.data
